60c5073a31823a176f0c4255e72fb052229f11e8
Martín Morondo <martinmorondo@gmail.com>
2023-04-10 23:51:06 -0300

    add services section with list of services and selected image

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -28,16 +28,57 @@ const Services = () => {
             description:
             "Usa nuestras instalaciones y equipo de fitness en un ambiente sin presiones",
             icon: "fas fa-map-signs",
-            image: "../assets/gym-3.jpg",
+            image: "https://mybayutcdn.bayut.com/mybayut/wp-content/uploads/Gyms-in-Mirdif-B-19-07.jpg",
         },
     ];
 
-    
     return (
-        <div>
+        <section id="servicios" className="bg-gray-900 py-10 my-10">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">
+              Servicios
+            </h2>
+            <div className="flex flex-wrap justify-center items-center">
+              <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0">
+                <ul className="list-none">
+                  {services.map((service) => (
+                    <li
+                      key={service.id}
+                      className={`flex items-center mb-4 p-4 rounded-lg shadow-lg transition-all duration-500 ${
+                        selectedService === service.id
+                          ? "bg-blue-500 text-white"
+                          : "bg-white text-gray-700"
+                      } hover:bg-blue-500 hover:text-white cursor-pointer`}
+                      onClick={() => {
+                        setSelectedService(service.id);
+                        setSelectedImage(service.image);
+                      }}
+                    >
+                      <div className="mr-4">
+                        <i className={`${service.icon} fa-2x`}></i>
+                      </div>
+                      <div>
+                        <h3 className="font-bold mb-2">{service.title}</h3>
+                        <p className="text-amber-950">{service.description}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="w-full md:w-1/2">
+                <img
+                  src={selectedImage}
+                  alt="Servicios"
+                  className="rounded-lg shadow-lg mb-4"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    };
+  
+  export default Services;
+    
             
-        </div>
-    );
-}
-
-export default Services;
+                
