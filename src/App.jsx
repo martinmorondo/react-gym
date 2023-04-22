1665a68fd39f6417cdc6c6f454107ddb9712ef69
Martín Morondo <martinmorondo@gmail.com>
2023-04-22 18:58:28 -0300

    add route and routes components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import Navbar from './components/NavBar';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
 
   return (
-   <div>
-      <Navbar />
-      <Home />
-      <Services />
-      <Testimonials />
-      <Contact />
-   </div>
+    <div>
+      <NavBar />
+      <Routes>
+      <Route exact path="/" element={ <Home /> } />
+      <Route exact path="/services" element={ <Services /> } />
+      <Route exact path="/testimonials" element={<Testimonials /> } />
+      <Route exact path="/contact" element={<Contact /> } />
+      </Routes>
+  </div>
   )
 }
 
